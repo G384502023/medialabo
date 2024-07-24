8e8e2c1de45e365f518f5ea28d731d2905209ead
G384502023 <134256161+G384502023@users.noreply.github.com>
2024-07-24 12:06:21 +0900

    a

--- weather.js
+++ weather.js
@@ -1,35 +1,32 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const btn = document.querySelector('#btn');
+  btn = document.querySelector('#btn');
   btn.addEventListener('click', sendRequest);
 });
 
-// 通信を開始する処理
 function sendRequest() {
-  const select = document.querySelector('#kensaku');
-  const cityId = select.value;
+ select = document.querySelector('#kensaku');
+ cityId = select.value;
 
 
 
-  // URL を設定
   const url = `https://www.nishita-lab.org/web-contents/jsons/openweather/${cityId}.json`;
 
-  // 通信開始
   axios.get(url)
-      .then(showResult)   // 通信成功
-      .catch(showError)   // 通信失敗
-      .then(finish);      // 通信の最後の処理
+      .then(showResult)   
+      .catch(showError)   
+      .then(finish);      
 }
 
-// 通信が成功した時の処理
+
 function showResult(resp) {
   const data = resp.data;
 
-  const weather = data.weather[0].description;
-  const tempMax = data.main.temp_max;
-  const tempMin = data.main.temp_min;
-  const humidity = data.main.humidity;
-  const windSpeed = data.wind.speed;
-  const windDirection = data.wind.deg;
+ weather = data.weather[0].description;
+ tempMax = data.main.temp_max;
+ tempMin = data.main.temp_min;
+ humidity = data.main.humidity;
+ windSpeed = data.wind.speed;
+ windDirection = data.wind.deg;
 
   const resultTable = document.querySelector('#weatherData');
   resultTable.innerHTML = `
@@ -44,13 +41,13 @@ function showResult(resp) {
   `;
 }
 
-// 通信エラーが発生した時の処理
+
 function showError(err) {
   console.log(err);
   alert('データの取得に失敗しました');
 }
 
-// 通信の最後にいつも実行する処理
+
 function finish() {
   console.log('Ajax 通信が終わりました');
 }
